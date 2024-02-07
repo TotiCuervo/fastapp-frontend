3bb835388b14d54f59e3a9a8594ebeb63a49a694
Ruben Cuervo <cuervor14@gmail.com>
2024-02-07 14:56:18 -0500

    fixing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Config } from 'tailwindcss'
 
 const config = {
@@ -87,25 +86,3 @@ const config = {
 } satisfies Config
 
 export default config
-=======
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
->>>>>>> e689d8c (Initial commit from Create Next App)
