615a41d5f16e67365a27b06ea919797c2dc6aea0
Ruben Cuervo <cuervor14@gmail.com>
2024-02-29 16:46:01 -0500

    testing out OG portfolio

--- src/app/(authenticated)/js/portfolio/[id]/page.tsx
+++ src/app/(authenticated)/js/portfolio/[id]/page.tsx
@@ -79,13 +79,10 @@ export default function page({ params }: IProps) {
 
     return (
         <div className="flex w-full flex-col gap-4">
-            <PageHeader portfolio={data} invalidation={bothInvalidation} />
-            <div className="flex flex-col gap-8 md:flex-row">
-                <div className="w-full md:w-4/12 lg:w-2/12">
-                    <PortfolioFilterItems route={(type) => portfolioIdRoute({ id, ...type })} />
-                </div>
+            <div className="flex justify-center">
                 <div className="w-full md:w-8/12 lg:w-8/12">
                     <div className="flex flex-col gap-10">
+                        <PageHeader portfolio={data} invalidation={bothInvalidation} />
                         <Section
                             title="Basic Information"
                             addModal={
